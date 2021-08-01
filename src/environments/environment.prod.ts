87dbdf1332e7b22880e59065168bb25d62423092
Diego <diegotgjeptha@gmail.com>
2021-08-01 12:11:52 +0200

    Adding Firebase to our app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyAkliv1HsEvtKVSnjcTnQv5fiPxL2mv78A",
+    authDomain: "udemytesting-aef1e.firebaseapp.com",
+    projectId: "udemytesting-aef1e",
+    storageBucket: "udemytesting-aef1e.appspot.com",
+    messagingSenderId: "1019774105093",
+    appId: "1:1019774105093:web:696f8bfa4b394bc14d8e6c",
+    measurementId: "G-6JL5LL9097"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAkliv1HsEvtKVSnjcTnQv5fiPxL2mv78A",
+    authDomain: "udemytesting-aef1e.firebaseapp.com",
+    projectId: "udemytesting-aef1e",
+    storageBucket: "udemytesting-aef1e.appspot.com",
+    messagingSenderId: "1019774105093",
+    appId: "1:1019774105093:web:696f8bfa4b394bc14d8e6c",
+    measurementId: "G-6JL5LL9097"
+  }
 };
 
 /*
